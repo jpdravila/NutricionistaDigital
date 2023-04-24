ad0a81bc276aa7944baec4224a6161490c20faf1
jpdravila <jpdravila@gmail.com>
2023-04-23 21:13:50 -0400

    Projeto inicial

---
+++ src/main/java/com/nutricionista/digital/NutricionistaDigital/NutricionistaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.nutricionista.digital.NutricionistaDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionistaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricionistaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutricionista/digital/NutricionistaDigital/NutricionistaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutricionista.digital.NutricionistaDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricionistaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
