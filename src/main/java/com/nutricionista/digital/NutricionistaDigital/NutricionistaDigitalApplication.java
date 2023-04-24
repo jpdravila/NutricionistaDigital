b275b18db497a0da761e2f3b72b9cb1df1f55098
jpdravila <jpdravila@gmail.com>
2023-04-23 21:55:53 -0400

    Adicionado propriedades de conexão com a Basa H2

--- src/main/java/com/nutricionista/digital/NutricionistaDigital/NutricionistaDigitalApplication.java
+++ src/main/java/com/nutricionista/digital/NutricionistaDigital/NutricionistaDigitalApplication.java
@@ -1,10 +1,19 @@
 package com.nutricionista.digital.NutricionistaDigital;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NutricionistaDigitalApplication {
+
+	@Bean
+	public CommandLineRunner execultar(){
+		return args -> {
+			System.out.println("Testando Spring");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NutricionistaDigitalApplication.class, args);
